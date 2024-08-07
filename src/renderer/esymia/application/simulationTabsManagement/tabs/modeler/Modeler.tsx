f0d30ab61ae83b0d65f7b0354ffcfb0f74345185
Draco112358 <gianni.frittella@gmail.com>
2024-08-07 10:08:06 +0200

    aggiunto aggiornamento progetto su fauna quando si importa un modello nel modeler

--- src/renderer/esymia/application/simulationTabsManagement/tabs/modeler/Modeler.tsx
+++ src/renderer/esymia/application/simulationTabsManagement/tabs/modeler/Modeler.tsx
@@ -9,10 +9,16 @@ import { GiAtomicSlashes, GiCubeforce } from 'react-icons/gi';
 import { useEffect, useState } from 'react';
 import { GrClone } from 'react-icons/gr';
 import { useStorageData } from '../simulator/rightPanelSimulator/hook/useStorageData';
-import { useSelector } from 'react-redux';
-import { SelectedFolderSelector, selectedProjectSelector } from '../../../../store/projectSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  SelectedFolderSelector,
+  selectedProjectSelector,
+} from '../../../../store/projectSlice';
 import { Folder, Project } from '../../../../model/esymiaModels';
 import { ImSpinner } from 'react-icons/im';
+import { useFaunaQuery } from 'cad-library';
+import { convertInFaunaProjectThis } from '../../../../faunadb/apiAuxiliaryFunctions';
+import { updateProjectInFauna } from '../../../../faunadb/projectsFolderAPIs';
 
 interface ModelerProps {
   selectedTabLeftPanel: string | undefined;
@@ -23,14 +29,26 @@ export const Modeler: React.FC<ModelerProps> = ({
   selectedTabLeftPanel,
   setSelectedTabLeftPanel,
 }) => {
-  useEffect(() => {
-    setSelectedTabLeftPanel(undefined)
-  },[])
+  const { cloneProject } = useStorageData();
+  const [cloning, setcloning] = useState<boolean>(false);
+  const selectedProject = useSelector(selectedProjectSelector);
+  const selectedFolder = useSelector(SelectedFolderSelector);
+  const { execQuery } = useFaunaQuery();
+  const dispatch = useDispatch();
 
-  const { cloneProject } = useStorageData()
-  const [cloning, setcloning] = useState<boolean>(false)
-  const selectedProject = useSelector(selectedProjectSelector)
-  const selectedFolder = useSelector(SelectedFolderSelector)
+  useEffect(() => {
+    setSelectedTabLeftPanel(undefined);
+  }, []);
+
+  useEffect(() => {
+    if (selectedProject?.model.components) {
+      execQuery(
+        updateProjectInFauna,
+        convertInFaunaProjectThis(selectedProject),
+        dispatch,
+      ).then(() => {});
+    }
+  }, [selectedProject?.model.components]);
 
   return (
     <div>
@@ -88,16 +106,29 @@ export const Modeler: React.FC<ModelerProps> = ({
       )}
       <div className="absolute left-[2%] top-[270px] rounded max-h-[500px] flex flex-col items-center gap-0 bg-white">
         <button
-          disabled={selectedProject && selectedProject.simulation && selectedProject.simulation.status === "Running"}
+          disabled={
+            selectedProject &&
+            selectedProject.simulation &&
+            selectedProject.simulation.status === 'Running'
+          }
           className={`p-2 tooltip rounded-t tooltip-right relative z-10 disabled:opacity-40`}
           data-tip="Clone Project"
           onClick={() => {
-            setcloning(true)
-            cloneProject(selectedProject as Project, selectedFolder as Folder, setcloning)
+            setcloning(true);
+            cloneProject(
+              selectedProject as Project,
+              selectedFolder as Folder,
+              setcloning,
+            );
           }}
         >
-          <GrClone style={{ width: '25px', height: '25px' }} className={`${cloning ? 'opacity-20' : 'opacity-100'}`} />
-          {cloning && <ImSpinner className="absolute z-50 top-3 bottom-1/2 animate-spin w-5 h-5" />}
+          <GrClone
+            style={{ width: '25px', height: '25px' }}
+            className={`${cloning ? 'opacity-20' : 'opacity-100'}`}
+          />
+          {cloning && (
+            <ImSpinner className="absolute z-50 top-3 bottom-1/2 animate-spin w-5 h-5" />
+          )}
         </button>
       </div>
     </div>
